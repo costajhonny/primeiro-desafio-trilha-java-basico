b0ccef96aa07680764c18dbd0c18155efd789e2a
costajhonny <officejc20@gmail.com>
2024-05-16 11:55:14 -0400

    primeiroPorjetoJavaDio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    
+   
+    public static void main(String[] args) throws Exception {      
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        int numeroDaConta;
+        double saldo;
+        String agencia,nomeDoCliente;
+        saldo = 237.48;
+
+        System.out.print("Por favor, digite o seu nome  !");
+        nomeDoCliente = sc.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência !");
+        agencia = sc.nextLine();
+
+        System.out.print("Por favor, digite o número da Conta !");
+        numeroDaConta = sc.nextInt();
+
+        System.out.printf(" Olá " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+        agencia + " Conta " + numeroDaConta + " e seu saldo " + saldo + " já está disponível para saque " );
+  
+       
     }
 }
